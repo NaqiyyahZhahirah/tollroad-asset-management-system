5ee46229a1e184f1a2510b46376d5cb9cc60b64c
NaqiyyahZhahirah <naqiyyah23001@mail.unpad.ac.id>
2026-09-01 09:21:06 +0700

    fix: carto api format

--- frontend/src/components/GeometryDrawer.jsx
+++ frontend/src/components/GeometryDrawer.jsx
@@ -154,7 +154,7 @@ export default function GeometryDrawer({ tipeGeometri, initialGeometry, onGeomet
             >
 
                 <TileLayer
-                    url={`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png?api_key=${import.meta.env.VITE_CARTO_API_KEY}`}
+                    url={`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png?key=${import.meta.env.VITE_CARTO_API_KEY}`}
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
                     subdomains="abcd"
                     maxZoom={19}
